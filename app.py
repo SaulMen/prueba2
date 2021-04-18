46a2336d0e9809bdab2eb801d2fd38237e66e3ff
SaulMen <3806336630101@ingenieria.usac.edu.gt>
2021-04-18 01:18:08 -0600

    creacion de ruta
    volviendo a inciar todo

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app =  Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def index():
+    return "<h1>Bienvenidos</h1>"
+
+if __name__=="__main__":
+    app.run(threaded=True, port=5001, debug=True)    
