1bf12ee3b970267d98a82a84de548176f6d52c96
SaulMen <3806336630101@ingenieria.usac.edu.gt>
2021-04-18 08:04:59 -0600

    Se creó la ruta "datetime"
    se creó la función datetime, además se importó una librería para la misma.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+from datetime import datetime as dt
 from flask import Flask
 from flask_cors import CORS
-
 app =  Flask(__name__)
 CORS(app)
 
@@ -12,6 +12,11 @@ def index():
 @app.route("/saludar")
 def saludar():
     return "<h1>Hola como estas</h1>"
+
+@app.route("/datetime")
+def datetime():
+    now_utc = dt.now()
+    return "<h1>"+str(now_utc)+"</h1>"
 
 if __name__=="__main__":
     app.run(threaded=True, port=5001, debug=True)    
